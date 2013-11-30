ad0777a607c5cde6fc3bf0024bd16fa4c236d357
walker <walker.shixy@gmail.com>
2013-11-30 21:52:42 +0800

    1.0

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-eoeh5
+EOE HTML5 Android客户端
 =====
 
-eoe html5版android客户端
+1. 本应用是基于html5+phonegap开发的EOE安卓客户端
 
-- 使用html5 + phonegap 开发
-- 功能基本参照eoe android开源版本
+2. 功能均是参考 <a href="https://github.com/eoecn/android-app" target="_blank" style="color:#2980B9 ">EOE开源安卓客户端</a>，感谢EOE开源安卓客户端所做的贡献。
 
-> 目前还不完善，持续开发中...
+3.  所有代码均已开源，有兴趣的朋友请自行fork,大家共同学习进步。
+
+4.  使用到的框架有：
+	
+> <a href="https://github.com/phonegap/phonegap" target="_blank" style="color: #2980B9;padding-left:20px;">phonegap(https://github.com/phonegap/phonegap)</a>
+
+> <a href="https://github.com/shixy/Jingle" target="_blank" style="color: #2980B9;padding-left:20px;">Jingle(https://github.com/shixy/Jingle)</a>
+
+> <a href="https://github.com/madrobby/zepto" target="_blank" style="color: #2980B9;padding-left:20px;">zepto(https://github.com/madrobby/zepto)</a>
+
+> <a href="https://github.com/cubiq/iscroll" target="_blank" style="color: #2980B9;padding-left:20px;">iScroll4(https://github.com/cubiq/iscroll)</a>
+
+> <a href="https://github.com/aui/artTemplate" target="_blank" style="color: #2980B9;padding-left:20px;">artTemplate(https://github.com/aui/artTemplate)</a>
+
+> <a href="https://github.com/shixy/BarcodeScanner" target="_blank" style="color: #2980B9;padding-left:20px;">zxing(BarcodeScanner)(https://github.com/shixy/BarcodeScanner)</a>
+
+<p style="font-weight: 600;color: #E74C3C">再次感谢所有开源的朋友们，因为有你们，世界变得更加美丽！</p>
+                    
--- assets/www/js/app/app.js
+++ assets/www/js/app/app.js
@@ -41,6 +41,7 @@ var App = (function(){
         Jingle.launch({
             showWelcome:false
         });
+        J.anim('body','scaleIn');
     };
     var page = function(id,factory){
         return ((id && factory)?_addPage:_getPage).call(this,id,factory);
@@ -196,29 +197,90 @@ App.page('index',function(){
     }
 });
 App.page('detail',function(){
-    var $container;
+    var $container,baseUrl;
     this.init = function(){
-        $container = $('#detail_article  div.scrollWrapper');
+        //$container = $('#detail_article  div.scrollWrapper');
+        $container = $('#detail_iframe');
+        $('#detail_section footer a').on('tap',function(){
+            _bindBar($(this));
+        });
     }
     this.load = function(){
         if(!this.url){
             console.error('没有获取数据url');
         }
-        $container.empty();
+        //$container.empty();
+        $container.attr('src','html/detail_frame.html');
         $('#detail_section header .title').text(this.title);
         J.showMask();
         EoeAPI.get(this.url,function(data){
-            $container.html(data.response.content);
+            $($container[0].contentWindow.document.body).html(data.response.content);
+            _renderBar(data.response);
             J.Scroll('#detail_article');
             J.hideMask();
         });
     }
+    var _renderBar = function(data){
+        var bar = data.bar;
+        var $good = $('#btn_userlike_good').data('url',bar.userlike.good);
+        var $bad = $('#btn_userlike_bad').data('url',bar.userlike.bad);
+        var $favo = $('#btn_user_favo').data('url',bar.favorite);
+        $('#comment_num').text(data.comment_num);
+        $('#btn_user_comment').data('url',bar.comment.get).data('submit',bar.comment.submit);
+        var good = localStorage.getItem('good-group')|| '';
+        var bad = localStorage.getItem('bad-group') || '';
+        var favo = localStorage.getItem('favo-group') || '';
+        baseUrl = data.share_url;
+        good.indexOf(baseUrl)>-1 ? $good.addClass('active-bar'):$good.removeClass('active-bar');
+        bad.indexOf(baseUrl)>-1 ? $bad.addClass('active-bar'):$bad.removeClass('active-bar');
+        favo.indexOf(baseUrl)>-1 ? $favo.addClass('active-bar'):$favo.removeClass('active-bar');
+    }
+    /**
+     * 绑定每个按钮的点击事件
+     * @param bar
+     * @private
+     */
+    var _bindBar = function(bar){
+        var id = bar.attr('id');
+        var url = bar.data('url');
+        var key = localStorage.getItem('authKey');
+        if(id == 'btn_user_comment'){
+            App.page('comment').getUrl = url;
+            App.page('comment').submitUrl = bar.data('submit');
+            J.Router.turnTo('#comment_section');
+        }else{
+            var isActive = bar.hasClass('active-bar');
+            var save_t = bar.data('save');
+            var storage = localStorage.getItem(save_t) || '';
+            if(isActive){
+                url = url.replace('add','del');
+            }
+            if(!key){
+                App.page('login').turnBack = true;
+                J.Router.turnTo('#login_section');
+                return;
+            }
+            EoeAPI.getWithSign(url,key,function(){
+                bar.toggleClass('active-bar');
+                if(isActive){//从本地存储中移除
+                    storage = storage.replace(baseUrl,'');
+                }else{//添加到本地
+                    storage += baseUrl;
+                }
+                localStorage.setItem(save_t,storage);
+            });
+        }
+    }
 });
 App.page('login',function(){
-    this.callback;
+    var _this = this;
+    this.turnBack = false;
     this.init = function(){
         $('#btn_login').tap(_login);
         $('#btn_qr_login').tap(_qr_login);
+        $('#login_section').on('pagehide',function(){
+            _this.turnBack = false;
+        })
     }
     var _login = function(){
         var username = $('#username').val();
@@ -232,12 +294,11 @@ App.page('login',function(){
                 var key = data.response.key;
                 if(key){
                     localStorage.setItem('authKey',key);
-                    if(this.callback){//处理未登录时需要登录处理的后续操作
-                        this.callback();
+                    if(_this.turnBack){//处理未登录时需要登录处理的后续操作
+                        J.Router.back();
                     }else{
                         J.Router.turnTo('#user_section');
                     }
-
                 }else{
                     $('#password').val('');
                     J.showToast(data.response.msg,'error');
@@ -247,6 +308,7 @@ App.page('login',function(){
     }
     var _qr_login = function(){
         window.plugins.barcodeScanner.scan('',function(result){
+            if(result.cancelled)return;
             var key = result.text;
             if(key.indexOf(':') == -1){
                 J.showToast('二维码不正确，无法登录','error');
@@ -255,13 +317,13 @@ App.page('login',function(){
             J.showMask('正在登录...');
             EoeAPI.getUserInfo(key,function(data){
                 J.hideMask();
-                if(data.response.isErr == 1){
+                if(data.response.isErr === 1){
                     J.showToast(data.response.msg,'error');
                 }else{
+                    localStorage.setItem('authKey',key);
                     App.page('user').userInfo = data;
-                    App.authKey = key;
-                    if(this.callback){//处理未登录时需要登录处理的后续操作
-                        this.callback();
+                    if(_this.turnBack){//处理未登录时需要登录处理的后续操作
+                        J.Router.back();
                     }else{
                         J.Router.turnTo('#user_section');
                     }
@@ -275,9 +337,6 @@ App.page('user',function(){
     this.userInfo = null;
     this.init = function(){
         var _this = this;
-//        $('#num_blog_favo,#num_news_favo').on('tap',function(){
-//            J.Router.turnTo('#favo_section');
-//        });
         $('#btn_logout').on('tap',function(){
             localStorage.removeItem('authKey');
             _this.userInfo = null;
@@ -319,4 +378,111 @@ App.page('favo',function(){
         J.tmpl('#favo_blog_article .list','tpl_favo',favo_data[0].lists);
         J.tmpl('#favo_news_article .list','tpl_favo',favo_data[1].lists);
     }
+});
+App.page('comment',function(){
+    var $list,$btnMore,moreUrl,$submit_container,$btn_login;
+    var _this = this;
+    this.getUrl = null;
+    this.submitUrl = null;
+    this.init = function(){
+        $list = $('#comment_article .list');
+        $btnMore = $list.next();
+        $submit_container = $('#comment_submit_container');
+        $btn_login = $('#btn_comment_login');
+        $btnMore.on('tap',function(){
+            if(!moreUrl)return;
+            _renderList();
+        });
+        $btn_login.on('tap',function(){
+            App.page('login').turnBack = true;
+            J.Router.turnTo('#login_section');
+        });
+        $('#btn_submit_comment').on('tap',function(){
+            var v = $('#input_comment').val();
+            if($.trim(v)==''){
+                J.showToast('评论不能为空');
+                return;
+            }
+            var url = _this.submitUrl + '&body='+v;
+            var key = localStorage.getItem('authKey');
+            J.showMask('正在提交');
+            EoeAPI.getWithSign(url,key,function(data){
+                J.hideMask();
+                if(data.response.isErr == 1){
+                    J.showToast('评论失败','error');
+                    return;
+                }else{
+                    var html = template('tpl_comment',[data.response.item]);
+                    $list.prepend(html);
+                    J.Scroll('#comment_article').scrollTo(0,0,0);
+                    J.showToast('评论成功','success');
+                }
+            });
+        });
+    }
+    this.show = function(){
+        var key = localStorage.getItem('authKey');
+        if(key){
+            $submit_container.show();
+            $btn_login.hide();
+        }else{
+            $submit_container.hide();
+            $btn_login.show();
+        }
+    }
+    this.load = function(){
+        _renderList(true);
+    }
+    var _renderList = function(showMask){
+        if(showMask){
+            moreUrl = null;
+            J.showMask();
+        }else{
+            $btnMore.text('加载中...');
+        }
+        EoeAPI.get(moreUrl||_this.getUrl,function(data){
+            moreUrl = data.response.more_url;
+            if(!moreUrl)$btnMore.hide();
+            if(data.response.items == ''){
+                J.Template.background($list,'亲，沙发还木有人哦..','drawer');
+                J.hideMask();
+                return;
+            }
+            J.tmpl($list,'tpl_comment',data.response.items,showMask?'replace':'add');
+            if(data.response.more_url){
+                $btnMore.show();
+            }else{
+                $btnMore.hide();
+            }
+            J.Scroll('#comment_article');
+            if(showMask){
+                J.hideMask();
+            }else{
+                $btnMore.text('点击加载更多');
+            }
+        });
+    }
+});
+//日期格式化
+Date.prototype.format = function(fmt)
+{ //author: meizz
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt))
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+    return fmt;
+}
+//注册方法到artTemplate中
+template.helper('dateFormat',function(time){
+    return new Date(parseInt(time)).format('yyyy-MM-dd hh:mm:ss');
 });
